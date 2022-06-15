3a49f14e37b758d8c6494d8f79d1cfde16f9988b
topwaye <107351010+topwaye@users.noreply.github.com>
2022-06-15 20:10:23 +0800

    Add files via upload

---
+++ Pseudo/types.h
@@ -0,0 +1,16 @@
+#ifndef TYPES_PSEUDO
+#define	TYPES_PSEUDO
+
+typedef int boolean_t;
+#ifndef TRUE
+#define	TRUE	1
+#endif
+#ifndef FALSE
+#define	FALSE	0
+#endif
+
+typedef unsigned long	count_t;
+typedef unsigned long	vaddr_t;
+typedef unsigned long	paddr_t;
+
+#endif
