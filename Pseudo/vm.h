16cb7677c9a8f0809a3dfcc4750445fd26d8040d
topwaye <107351010+topwaye@users.noreply.github.com>
2022-06-15 17:56:49 +0800

    Update vm.h

--- Pseudo/vm.h
+++ Pseudo/vm.h
@@ -7,10 +7,10 @@ typedef unsigned long	vsize_t;
 typedef unsigned long	paddr_t;
 
 /* Tail queue definitions */
-#define TAILQ_HEAD(name, type)										\
-struct name 														\
-{																	\
-	struct type *tqh_first;	/* first element */						\
+#define TAILQ_HEAD(name, type)							\
+struct name 									\
+{										\
+	struct type *tqh_first;	/* first element */				\
 	struct type **tqh_last;	/* addr of last next element */			\
 }
 
