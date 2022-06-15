b07f95db3516f0ecef08776438008f1c03ff28fa
topwaye <107351010+topwaye@users.noreply.github.com>
2022-06-15 22:18:30 +0800

    Update vm.h

--- Pseudo/vm.h
+++ Pseudo/vm.h
@@ -5,31 +5,32 @@ TAILQ_HEAD(PGLIST, VMPAGE);
 TAILQ_HEAD(HASHLIST, VMPAGE);
 TAILQ_HEAD(MAPENTRYLIST, VMMAPENTRY);
 
+
 struct VMPAGE
 {
+	simple_lock_data_t		lock;		/* page can be held by other processes */
 	boolean_t			copying;	/* PG_BUSY: page is being copied */
 	count_t				refs;		/* reference count */
 	vaddr_t				offset;		/* offset into object */
 	paddr_t				physaddr;	/* physical address of page */
 };
 
-struct VMOBJECT
-{	
+struct VMOBJECT						/* there is no reference count */
+{							/* cannot be held by other processes */
 	struct HASHLIST			hashq;		/* hash table links */
 	struct PGLIST			memq;		/* pages in this object */
 	count_t				pages;		/* number of pages in memq */
-	count_t				refs;		/* reference count */
 };
 
 union VMMAPOBJECT
 {
 	VMMAP				submap;		/* belongs to another map */
 	struct
-	{
+	{	
 		count_t	offset;				/* offset into the main object */
-		struct VMOBJECT* main;			/* object I point to */
-		struct VMOBJECT* copy;			/* anonymous overlay */
-	}				object;
+		struct VMOBJECT	main;			/* doesn't allow to hold another object */
+		struct VMOBJECT	copy;			/* anonymous overlay */
+	}						object;
 };
 
 struct VMMAPENTRY
