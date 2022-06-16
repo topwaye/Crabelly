8b63869e93acde8cb50498d9633ec42368d03ecf
topwaye <107351010+topwaye@users.noreply.github.com>
2022-06-16 13:32:39 +0800

    Update queue.h

--- Pseudo/queue.h
+++ Pseudo/queue.h
@@ -4,9 +4,9 @@
 /*
  * Tail queue definitions.
  */
-#define TAILQ_HEAD(name, type)						            \
-struct name {								                    \
-	struct type *tqh_first;	/* first element */			        \
+#define TAILQ_HEAD(name, type)						\
+struct name {								\
+	struct type *tqh_first;	/* first element */			\
 	struct type **tqh_last;	/* addr of last next element */		\
 }
 
