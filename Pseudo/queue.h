e9fa40ca28b708f3a66f47a4fb4b6dcb51e591e2
topwaye <107351010+topwaye@users.noreply.github.com>
2022-06-16 13:32:03 +0800

    Add files via upload

---
+++ Pseudo/queue.h
@@ -0,0 +1,13 @@
+#ifndef	QUEUE_PSEUDO
+#define	QUEUE_PSEUDO
+
+/*
+ * Tail queue definitions.
+ */
+#define TAILQ_HEAD(name, type)						            \
+struct name {								                    \
+	struct type *tqh_first;	/* first element */			        \
+	struct type **tqh_last;	/* addr of last next element */		\
+}
+
+#endif	/* QUEUE_PSEUDO */
