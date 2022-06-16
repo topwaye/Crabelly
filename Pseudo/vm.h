fdb48dc2839de26367c9162df1093b06d287fcdb
topwaye <107351010+topwaye@users.noreply.github.com>
2022-06-16 13:30:45 +0800

    Update vm.h

--- Pseudo/vm.h
+++ Pseudo/vm.h
@@ -1,37 +1,38 @@
 #ifndef VM_PSEUDO
 #define VM_PSEUDO
 
-struct VMSEGMENT
-{
-	count_t				offset;		/* offset into pager */
-	count_t				copyings;	/* number of copyings */
-	vaddr_t				copying[16];	/* pages being copied */
+TAILQ_HEAD(PGLIST, vaddr_t); 
+
+struct VMSEGMENT							/* pages do exist, but */
+{									/* VMPAGE struct does NOT exist */
+	count_t			offset;					/* offset into pager */
+	struct PGLIST		copyingq;				/* pages being copied, more than CPU count */
 };
 
 union VMMAPSEG
 {
-	VMMAP				submap;		/* belongs to another map */
-	VMSEGMENT			segment;
+	VMMAP			submap;					/* belongs to another map */
+	VMSEGMENT		segment;
 };
 
 struct VMMAPENTRY
 {
-	vaddr_t				start;		/* start address */
-	vaddr_t				end;		/* end address */
-	union VMMAPSEG			mapseg;
+	vaddr_t			start;					/* start address */
+	vaddr_t			end;					/* end address */
+	union VMMAPSEG		mapseg;
 };
 
 struct VMMAP
 {
-	struct MAPENTRYLIST		entryq;		/* list of entries */
-	count_t				entries;	/* number of entries */
+	struct MAPENTRYLIST	entryq;					/* list of entries */
+	count_t			entries;				/* number of entries */
 };
 
 struct VMSPACE
 {
-	simple_lock_data_t		lock;		/* lock for the entire treemap */
-	struct VMMAP			map;		/* virtual map */
-	struct PHYSMAP*			pmap;		/* physical map */
+	simple_lock_data_t	lock;					/* lock for the entire treemap */
+	struct VMMAP		map;					/* virtual map */
+	struct PHYSMAP*		pmap;					/* physical map */
 };
 
 #endif
