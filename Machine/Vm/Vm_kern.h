897f644ead379ea8d9572e646f74fda018458c9a
topwaye <107351010+topwaye@users.noreply.github.com>
2022-06-19 20:53:14 +0800

    Add files via upload

---
+++ Machine/Vm/Vm_kern.h
@@ -0,0 +1,10 @@
+/* Machine: vm_kern.h,v 1.00 2022/06/19 */
+
+/* Kernel memory management definitions. */
+
+extern vm_map_t	buffer_map;
+extern vm_map_t	exec_map;
+extern vm_map_t	kernel_map;
+extern vm_map_t	kmem_map;
+extern vm_map_t	mb_map;
+extern vm_map_t	phys_map;
