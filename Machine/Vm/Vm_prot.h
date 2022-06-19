f7e7af1ba59d0fd9cc3877268c680c0fcd8a1307
topwaye <107351010+topwaye@users.noreply.github.com>
2022-06-19 18:54:51 +0800

    Add files via upload

---
+++ Machine/Vm/Vm_prot.h
@@ -0,0 +1,40 @@
+/* Machine: vm_prot.h,v 1.00 2022/06/19 */
+
+/*
+ *	Virtual memory protection definitions.
+ */
+
+#ifndef	_VM_PROT_
+#define	_VM_PROT_
+
+/*
+ *	Types defined:
+ *
+ *	vm_prot_t		VM protection values.
+ */
+
+typedef int		vm_prot_t;
+
+/*
+ *	Protection values, defined as bits within the vm_prot_t type
+ */
+
+#define	VM_PROT_NONE	((vm_prot_t) 0x00)
+
+#define VM_PROT_READ	((vm_prot_t) 0x01)	/* read permission */
+#define VM_PROT_WRITE	((vm_prot_t) 0x02)	/* write permission */
+#define VM_PROT_EXECUTE	((vm_prot_t) 0x04)	/* execute permission */
+
+/*
+ *	The default protection for newly-created virtual memory
+ */
+
+#define VM_PROT_DEFAULT	(VM_PROT_READ|VM_PROT_WRITE|VM_PROT_EXECUTE)
+
+/*
+ *	The maximum privileges possible, for parameter checking.
+ */
+
+#define VM_PROT_ALL	(VM_PROT_READ|VM_PROT_WRITE|VM_PROT_EXECUTE)
+
+#endif /* _VM_PROT_ */
