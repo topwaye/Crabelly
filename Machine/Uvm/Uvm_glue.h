8421615b33ccc1e70ba41936a8f36788956952fc
topwaye <107351010+topwaye@users.noreply.github.com>
2022-06-20 21:13:30 +0800

    Add files via upload

---
+++ Machine/Uvm/Uvm_glue.h
@@ -0,0 +1,12 @@
+/* Machine: uvm_glue.h,v 1.00 2022/06/20 */
+
+#ifndef _UVM_UVM_GLUE_H_
+#define _UVM_UVM_GLUE_H_
+
+/*
+ * uvm_glue.h
+ */
+
+void uvm_swapout_threads __P((void));
+
+#endif /* _UVM_UVM_GLUE_H_ */
