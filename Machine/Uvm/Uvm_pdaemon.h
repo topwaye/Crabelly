5d7612d61776cced7f584c5437bbf3ebebab7da3
topwaye <107351010+topwaye@users.noreply.github.com>
2022-06-20 21:34:12 +0800

    Add files via upload

---
+++ Machine/Uvm/Uvm_pdaemon.h
@@ -0,0 +1,16 @@
+/* Machine: uvm_pdaemon.h,v 1.00 2022/06/20 */
+
+#ifndef _UVM_UVM_PDAEMON_H_
+#define _UVM_UVM_PDAEMON_H_
+
+/*
+ * uvm_pdaemon.h: page daemon hooks
+ */
+
+/*
+ * prototypes
+ */
+
+void uvm_wait __P((char *));
+
+#endif /* _UVM_UVM_PDAEMON_H_ */
